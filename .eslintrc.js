e1b0ff26af3a06235e08badadbe083257792224b
NullVoxPopuli <LPSego3+dev@gmail.com>
2021-07-24 11:41:57 -0400

    chore(internal): re-apply ember blueprint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,4 @@ const { configs } = require('@nullvoxpopuli/eslint-configs');
 
 const config = configs.ember();
 
-module.exports = {
-  ...config,
-  overrides: [...config.overrides],
-};
+module.exports = { ...config, overrides: [...config.overrides] };
